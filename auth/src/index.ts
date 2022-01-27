4f8ccb2a7ad20f2f36cc6e4d5b8f92162eb8d4ae
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-01-27 12:14:37 +0200

    chore: add ci - ingress and skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express, { json } from 'express'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hello')
+})
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000, auth.')
+  console.log('Listening on port 3000, auth')
 })
