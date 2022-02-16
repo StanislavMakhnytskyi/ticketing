a220e6c97e649a04959bcd3485b7edd64b1210d1
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-02-16 18:15:44 +0300

    chore: add global css

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -4,11 +4,7 @@ import request from 'supertest'
 import { app } from '../app'
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): Promise<string[]>
-    }
-  }
+    function signin(): Promise<string[]>;
 }
 
 let mongo: any
@@ -50,9 +46,7 @@ global.signin = async () => {
     })
     .expect(201)
 
-  return response.get('Set-Cookie')
-}
+  const cookie = response.get('Set-Cookie')
 
-declare global {
-  var signin: () => Promise<string[]>
+  return cookie
 }
