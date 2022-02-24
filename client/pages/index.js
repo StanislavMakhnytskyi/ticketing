4db34f6a90ce053e8580cfb154396e558d0bb669
StanislavMakhnytskyi <makhnitskiy.stanislav@gmail.com>
2022-02-24 13:18:13 +0300

    chore: implement global header

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,8 @@
 import buildClient from '../api/build-client'
 
-const Landing = ({ currentUser }) => <h1>Landing Page</h1>
+const Landing = ({ currentUser }) => (
+  <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>
+)
 
 Landing.getInitialProps = async (context) => {
   const client = buildClient(context)
